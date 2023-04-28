d3054bdcc41c9f164d442a12bec03dda86944100
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-04-28 10:04:37 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -146,6 +146,9 @@ def _clone(message, bot):
             if FORCE_BOT_PM is False:
                 upldmsg = sendMarkup(result + cc, bot, message, button)
                 Thread(target=auto_delete_upload_message, args=(bot, message, upldmsg)).start()
+        if is_share:
+            LOGGER.info(f"Deleting: {link}")
+            gd.deletefile(link)
         if MIRROR_LOGS:
             try:
                 for chatid in MIRROR_LOGS:
